aa8965b676ee943b2545bd4de752fcb45e311be8
Akash S Pillai <akash@Akashs-Air.lan>
2026-06-01 07:15:40 +0530

    updated Todo

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,44 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
+import Todo from "./Todo";
 
 const App = () => {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+//add a task 
+const addTodo = () => {
+  if(task.trim() === "") return;
+
+  setTodos((prev) => [...prev, task]);
+  setTask("")
+};
+//delete task
+  const deleteTodo = (indexToDelete) => {
+    setTodos((prev) =>
+      prev.filter((_, index) => index !== indexToDelete)
+    );
+  };
+
+  const handleChange =  (e) => {
+    setTask(e.target.value)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>To-Do-List</h1>
+
+      <input
+      type="text"
+      placeholder="Enter task"
+      value={task}
+      onChange={handleChange}
+      />
+  
+       <button onClick={addTodo}>Add Todo</button>
+
+      <Todo todos={todos} deleteTodo={deleteTodo} />
     </div>
   )
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Todo = ({ todos, deleteTodo}) => {
+  return (
+    <div>
+        <ul>
+            {
+                todos.map((todo, index) =>(
+                    <li key={index}>{todo}
+                      <button onClick={() =>deleteTodo(index)}>Delete</button>
+                    </li>
+                    
+                ))
+            }
+        </ul>
+    </div>
+  )
+}
+
+export default Todo
